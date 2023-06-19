a3d155b72ac7071d5fc5b07515f60ac6a0671c71
MContour <m-contour@qq.com>
2023-06-19 23:53:10 +0800

    feat: chapter1: support function call in libOS

--- ch1/os/src/entry.asm
+++ ch1/os/src/entry.asm
@@ -2,4 +2,12 @@
     .section .text.entry
     .global _start
 _start:
-    li x1, 100
+    la sp, boot_stack_top
+    call rust_main
+
+    .section .bss.stack
+    .global boot_stack_lower_bound
+boot_stack_lower_bound:
+    .space 4096 * 16
+    .global boot_stack_top
+boot_stack_top:
--- ch1/os/src/main.rs
+++ ch1/os/src/main.rs
@@ -7,6 +7,24 @@ use core::arch::global_asm;
 
 global_asm!(include_str!("entry.asm"));
 
-fn main() {
+#[no_mangle]
+fn rust_main() {
     // do nothing
+    clear_bss();
+    loop {}
+}
+
+fn clear_bss() {
+    extern "C" {
+        static mut sbss: u64;
+        static mut ebss: u64;
+    }
+
+    unsafe {
+        (sbss as usize..ebss as usize).for_each(|ptr|{
+                // use volatile to avoid compiler optimization
+                (ptr as *mut u8).write_volatile(0);
+            }
+        );
+    }
 }

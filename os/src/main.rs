8cb38c7672749c68cd175633481232e56a26b690
MContour <m-contour@qq.com>
2023-06-25 10:51:35 +0800

    feat: fix no_std bugs and add some new components

--- os/src/main.rs
+++ os/src/main.rs
@@ -1,6 +1,9 @@
-#![feature(panic_info_message)]
 #![no_std] // tell rustc not use the standard library
 #![no_main] // the simplest way to disable the 'start' program to initialize env
+#![feature(panic_info_message)]
+#![reexport_test_harness_main = "test_main"]
+#![feature(custom_test_frameworks)]
+#![test_runner(test_runner)]
 
 mod lang_items;
 mod sbi;
@@ -69,4 +72,8 @@ fn rust_main() {
 
     println!("Hello, world!");
     shutdown();
+}
+
+pub fn test_runner(_test: &[&i32]) {
+    loop {}
 }
---
+++ user/src/lang_items.rs
@@ -0,0 +1,19 @@
+// os/src/lang_items.rs
+use core::panic::PanicInfo;
+use crate::println;
+use crate::sbi::shutdown;
+
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    if let Some(location) = info.location() {
+        println!("Panicked at {}:{} {}",
+            location.file(),
+            location.line(),
+            info.message().unwrap()
+            
+        );
+    } else {
+        println!("Panicked: {}", info.message().unwrap());
+    }
+    shutdown()
+}

8fbb8e0f506d23ae958414bf085a2580b29e4472
MContour <m-contour@qq.com>
2023-06-27 22:34:45 +0800

    feat: ch2=>batch.rs: wrap USER_STACK and KERNEL_STACK with UnSafeCell

--- os/src/batch.rs
+++ os/src/batch.rs
@@ -3,6 +3,7 @@
 use crate::sbi::shutdown;
 use crate::sync::UPSafeCell;
 use crate::trap::TrapContext;
+use core::cell::UnsafeCell;
 use core::arch::asm;
 use lazy_static::*;
 
@@ -14,37 +15,37 @@ const APP_SIZE_LIMIT: usize = 0x20000;
 
 #[repr(align(4096))]
 struct KernelStack {
-    data: [u8; KERNEL_STACK_SIZE],
+    data: UnsafeCell<[u8; KERNEL_STACK_SIZE]>,
 }
 
 #[repr(align(4096))]
 struct UserStack {
-    data: [u8; USER_STACK_SIZE],
+    data: UnsafeCell<[u8; KERNEL_STACK_SIZE]>,
 }
 
 static mut KERNEL_STACK: KernelStack = KernelStack {
-    data: [0; KERNEL_STACK_SIZE],
+    data: UnsafeCell::new([0; KERNEL_STACK_SIZE]),
 };
 static mut USER_STACK: UserStack = UserStack {
-    data: [0; USER_STACK_SIZE],
+    data: UnsafeCell::new([0; KERNEL_STACK_SIZE]),
 };
 
 impl KernelStack {
-    fn get_sp(&self) -> usize {
-        self.data.as_ptr() as usize + KERNEL_STACK_SIZE
+    fn get_sp(&self) -> *mut u8 {
+            (self.data.get() as *mut u8).wrapping_add(KERNEL_STACK_SIZE)
     }
     pub fn push_context(&self, cx: TrapContext) -> &'static mut TrapContext {
-        let cx_ptr = (self.get_sp() - core::mem::size_of::<TrapContext>()) as *mut TrapContext;
+        let cx_ptr = (self.get_sp().wrapping_sub( core::mem::size_of::<TrapContext>())) as *mut TrapContext;
         unsafe {
             *cx_ptr = cx;
+            cx_ptr.as_mut().unwrap()
         }
-        unsafe { cx_ptr.as_mut().unwrap() }
     }
 }
 
 impl UserStack {
-    fn get_sp(&self) -> usize {
-        self.data.as_ptr() as usize + USER_STACK_SIZE
+    fn get_sp(&self) -> *mut u8 {
+        (self.data.get() as *mut u8).wrapping_add(USER_STACK_SIZE)
     }
 }
 
@@ -147,7 +148,7 @@ pub fn run_next_app() -> ! {
     unsafe {
         __restore(KERNEL_STACK.push_context(TrapContext::app_init_context(
             APP_BASE_ADDRESS,
-            USER_STACK.get_sp(),
+            USER_STACK.get_sp() as *const _ as usize,
         )) as *const _ as usize);
     }
     panic!("Unreachable in batch::run_current_app!");
